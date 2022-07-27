d3cf386a3b5dacf5eaca2387c88cf0c297836904
Rojin <rojin.tarigh73@gmail.com>
2022-07-27 23:10:07 +0430

    completed image gallery

---
+++ app.js
@@ -0,0 +1,21 @@
+function imageGallery() {
+	const hightlight = document.querySelector(".gallery-highlight");
+	const previews = document.querySelectorAll(".room-preview img");
+
+	previews.forEach(preview => {
+		preview.addEventListener("click", function() {
+			const smallSrc = this.src;
+			
+			const bigSrc = smallSrc.replace("small", "big");
+			hightlight.src= bigSrc;
+			previews.forEach(preview => preview.classList.remove("room-active"));
+			preview.classList.add('room-active');
+
+		});
+
+	});
+
+}
+
+imageGallery();
+
